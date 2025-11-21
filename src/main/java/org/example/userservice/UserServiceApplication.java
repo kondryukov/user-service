2ba4ac866486df9d7d553ad2937e6ddc76aae5c5
mark <kondryokov@gmail.com>
2025-11-21 19:58:42 +0300

    small fix

--- src/main/java/org/example/userservice/UserServiceApplication.java
+++ src/main/java/org/example/userservice/UserServiceApplication.java
@@ -1,8 +1,5 @@
 package org.example.userservice;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -15,14 +12,5 @@ public class UserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner printKafkaProps(KafkaProperties props) {
-        return args -> {
-            System.out.println("=== Effective producer props ===");
-            props.buildProducerProperties()
-                    .forEach((k, v) -> System.out.println(k + " = " + v));
-        };
     }
 }
